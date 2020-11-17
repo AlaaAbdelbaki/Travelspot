8cb9481da89b9c7ba7e40c662b0a32a9a7ed9fd9
AlaaAbdelbaki <alaa.abdelbaki@esprit.tn>
2020-11-17 01:15:18 +0100

    Added splash scrren

--- app/src/main/java/com/travelspot/travelspot/MainActivity.java
+++ app/src/main/java/com/travelspot/travelspot/MainActivity.java
@@ -4,10 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
