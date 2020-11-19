8eb72fbee259d9dcaaad1a48e391fda8e1bd5aff
Trabelsi Omar <omar.trabelsi.1@esprit.tn>
2020-11-19 17:25:27 +0100

    Added First Screen

--- app/src/main/java/com/travelspot/travelspot/MainActivity.java
+++ app/src/main/java/com/travelspot/travelspot/MainActivity.java
@@ -17,6 +17,6 @@ public class MainActivity extends AppCompatActivity {
         }
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_first);
     }
 }
